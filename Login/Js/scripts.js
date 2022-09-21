61cbc2521f5c6f34c814b84f1163d7ee8fd77a31
Felipe Gomes <felipegomespsg@hotmail.com>
2022-09-20 23:21:45 -0300

    update html

--- Login/Js/scripts.js
+++ Login/Js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Teste")
+// Seleção de elementos
+    const generatePasswordButton = document.querySelector("#generate-password");
+    const generatedPasswordElement = document.querySelector("#geneted-password");
+// Funções
+// Letras, Números e Símbolos
+
+
+
+
+// Eventos
+    generatePasswordButton.addEventListener("click", () => {
+        console.log("teste");
+    })
