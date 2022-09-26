1f0dfbe808120a474f22b8e37da859384ea5a5b5
Felipe Gomes <felipegomespsg@hotmail.com>
2022-09-26 00:17:18 -0300

    Update Scripts
    Finalização da parte de scripts

--- Login/Js/scripts.js
+++ Login/Js/scripts.js
@@ -1,9 +1,8 @@
 // Seleção de elementos
     const generatePasswordButton = document.querySelector("#generate-password");
-    const generatedPasswordElement = document.querySelector("#geneted-password");
+    const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Funções
-// Letras, Números e Símbolos
 const getLetterLowerCase = () => {
      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
@@ -12,12 +11,46 @@ const getLetterUpperCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
-getLetterLowerCase();
-getLetterUpperCase();
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
 
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%&*";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
 
+const generatedPassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = "";
+
+    const passwordLength = 10
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    ]
+
+    for(i =0 ; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+            password += randomValue;
+        });
+    }
+
+    password = password.slice(0, password.length);
+    
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+};
 
 // Eventos
     generatePasswordButton.addEventListener("click", () => {
-        console.log("teste");
+        generatedPassword(
+            getLetterLowerCase,
+            getLetterUpperCase,
+            getNumber,
+            getSymbol
+        );
     })
