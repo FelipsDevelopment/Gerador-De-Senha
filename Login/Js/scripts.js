2151f2f263ac1924a54d85e6cbdc1871edffd4d7
Felipe Gomes <felipegomespsg@hotmail.com>
2022-09-25 23:44:09 -0300

    Update Scripts

--- Login/Js/scripts.js
+++ Login/Js/scripts.js
@@ -1,9 +1,19 @@
 // Seleção de elementos
     const generatePasswordButton = document.querySelector("#generate-password");
     const generatedPasswordElement = document.querySelector("#geneted-password");
+
 // Funções
 // Letras, Números e Símbolos
+const getLetterLowerCase = () => {
+     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+getLetterLowerCase();
+getLetterUpperCase();
 
 
 
